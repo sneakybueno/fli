d9c7cf95dcdeef1e2b09252d9ffe0e616b56a388
Eric Bueno <eric.bueno23@gmail.com>
2017-01-01 20:19:21 -0500

    Adding tests for directory commands

---
+++ fuego/fstore_test.go
@@ -0,0 +1,56 @@
+package fuego_test
+
+import (
+	"testing"
+
+	"github.com/sneakybueno/fli/fuego"
+)
+
+const (
+	firebaseTestingURL = "https://go-fli.firebaseio.com/"
+)
+
+func TestDirectoryCommands(t *testing.T) {
+	fStore := fuego.NewFStore(firebaseTestingURL)
+
+	expected := "/"
+	wd := fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+
+	fStore.Cd("users")
+	expected = "/users"
+	wd = fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+
+	fStore.Cd("bueno/dev")
+	expected = "/users/bueno/dev"
+	wd = fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+
+	fStore.Cd("..")
+	expected = "/users/bueno"
+	wd = fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+
+	fStore.Cd("../corgi")
+	expected = "/users/corgi"
+	wd = fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+
+	fStore.Cd("../..")
+	expected = "/"
+	wd = fStore.Wd()
+	if wd != expected {
+		t.Errorf("Expected %s, got %s", expected, wd)
+	}
+}
