2c45564a83b50ad38fa6bb9d3c49c415504457a5
Eric Bueno <eric.bueno23@gmail.com>
2017-01-03 22:50:47 -0500

    [#2] Adding command line support
    Also removed global fStore and using FStore that supports auth

--- cmd/fli/main.go
+++ cmd/fli/main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
-
 	"strings"
 
-	"errors"
+	"flag"
 
 	"github.com/sneakybueno/fli/fuego"
 )
 
-var fStore *fuego.FStore
-
 func main() {
-	firebaseURL := "https://go-fli.firebaseio.com/"
-	fStore = fuego.NewFStore(firebaseURL)
+	var firebaseURL string
+	var serviceAccountPath string
+
+	flag.StringVar(&firebaseURL, "host", "", "Firebase database URL (Required)")
+	flag.StringVar(&serviceAccountPath, "config", "", "Path to service account file (Required)")
+	flag.Parse()
+
+	if firebaseURL == "" || serviceAccountPath == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fStore, err := fuego.NewFStore(firebaseURL, serviceAccountPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	fmt.Printf("Time to fli @ %s\n", fStore.WorkingDirectoryURL())
 	fmt.Print(fStore.Prompt())
@@ -25,7 +38,7 @@ func main() {
 	for scanner.Scan() {
 		input := scanner.Text()
 
-		m, err := processInput(input)
+		m, err := processInput(fStore, input)
 		if err != nil {
 			fmt.Println(err)
 		} else if m != "" {
@@ -36,7 +49,7 @@ func main() {
 	}
 }
 
-func processInput(input string) (string, error) {
+func processInput(fStore *fuego.FStore, input string) (string, error) {
 	if len(input) == 0 {
 		return "", nil
 	}
@@ -57,8 +70,7 @@ func processInput(input string) (string, error) {
 	case "ls":
 		return fStore.Ls()
 	case "pwd":
-		m := fmt.Sprintf("%s", fStore.WorkingDirectoryURL())
-		return m, nil
+		return fStore.WorkingDirectoryURL(), nil
 	default:
 		message := fmt.Sprintf("command not found: %s", command)
 		return "", fliError(message)
